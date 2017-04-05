cc2667f656435a793210d170bf9cbc935a50c5e7
jiziqiang <jiziqiang@eshippinggateway.com>
2017-04-05 17:05:58 +0800

    项目

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var userDao=require('../dao/userDao')
+var userDao=require('../dao/userDao');
 var router = express.Router();
 
 /* GET home page. */
@@ -12,10 +12,19 @@ router.get('/', function(req, res, next) {
     res.render('home',option);
   })
 });
-router.get('/index', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   var option={};
   option.cssList= ['home.css'];
   option.jsList= ['home.js'];
+  option.template=
+      '<script id="home-template" type="text/x-handlebars-template" class="templateList">'+
+        '<div class="entry">'+
+           '<h1>{{title}}</h1>'+
+           '<div class="body">'+
+              '{{body}}'+
+           '</div>'+
+        '</div>'+
+      '</script>';
   userDao.login(function(val){
     option.val=val;
     res.json(option);
@@ -26,24 +35,60 @@ router.get('/shop', function(req, res, next) {
   var option={};
   option.cssList= ['shop.css'];
   option.jsList= ['shop.js'];
+  option.template=
+      '<script id="shop-template" type="text/x-handlebars-template" class="templateList">'+
+      '<div class="entry">'+
+      '<h1>{{title}}</h1>'+
+      '<div class="body">'+
+      '{{body}}'+
+      '</div>'+
+      '</div>'+
+      '</script>';
   res.json(option);
 });
 router.get('/store', function(req, res, next) {
   var option={};
   option.cssList= ['store.css'];
   option.jsList= ['store.js'];
+  option.template=
+      '<script id="store-template" type="text/x-handlebars-template" class="templateList">'+
+      '<div class="entry">'+
+      '<h1>{{title}}</h1>'+
+      '<div class="body">'+
+      '{{body}}'+
+      '</div>'+
+      '</div>'+
+      '</script>';
   res.json(option);
 });
 router.get('/shop-cart', function(req, res, next) {
   var option={};
   option.cssList= ['shop-cart.css'];
   option.jsList= ['shop-cart.js'];
+  option.template=
+      '<script id="shop-cart-template" type="text/x-handlebars-template" class="templateList">'+
+      '<div class="entry">'+
+      '<h1>{{title}}</h1>'+
+      '<div class="body">'+
+      '{{body}}'+
+      '</div>'+
+      '</div>'+
+      '</script>';
   res.json(option);
 })
 ;router.get('/mine', function(req, res, next) {
   var option={};
   option.cssList= ['mine.css'];
   option.jsList= ['mine.js'];
+  option.template=
+      '<script id="mine-template" type="text/x-handlebars-template" class="templateList">'+
+      '<div class="entry">'+
+      '<h1>{{title}}</h1>'+
+      '<div class="body">'+
+      '{{body}}'+
+      '</div>'+
+      '</div>'+
+      '</script>';
   res.json(option);
 });
 module.exports = router;
