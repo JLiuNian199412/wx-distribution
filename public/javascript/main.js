91e68e5fb69cdbc50f0082011ba5243c45c85bbc
jiziqiang <jiziqiang@eshippinggateway.com>
2017-04-17 18:51:30 +0800

    项目

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -8,7 +8,7 @@ function showTemplate(url,templates,left,middle,right){
     })
 }
 var ui={};
-ui.header='<a class="aui-pull-left aui-btn"></a> <div class="aui-title"></div><a class="aui-pull-right aui-btn "></a>';
+ui.header='<a class="aui-pull-left aui-btn"></a><div class="aui-title"></div><a class="aui-pull-right aui-btn "></a>';
 ui.search='<div class="aui-searchbar" id="search">'+
             '<div class="aui-searchbar-input aui-border-radius" tapmode>'+
                  '<i class="aui-iconfont aui-icon-search"></i>'+
@@ -19,4 +19,5 @@ ui.search='<div class="aui-searchbar" id="search">'+
         '</div>';
 ui.menu= '<span class="aui-iconfont aui-icon-menu"></span>';
 ui.comment= '<span class="aui-iconfont aui-icon-comment"></span>';
+ui.left= '<span class="aui-iconfont aui-icon-left"></span>';
 
